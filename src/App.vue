0ae3a58e7649331f8ded52705561afe57317f97b
paopaolaowang <56791068@qq.com>
2020-03-21 23:11:49 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <my-header/>
+    <router-view/>
+    <my-footer/>
+  </div>
+</template>
+<script>
+  import myFooter from '@/components/Footer'
+  import myHeader from '@/components/Header'
+  export default {
+    components: {
+      myFooter,
+      myHeader
+    },
+  }
+</script>
+<style lang="scss">
+@import './assets/layui/css/layui.css';
+</style>
---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.resolve(__dirname,dir)
+}
+
+module.exports = {
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+}
